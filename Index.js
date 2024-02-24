6d8a4752185fe98eeb0355376999a2e33ed36cc4
HIMANSHU KUMAR SINGH <113089207+Himanshu-Kr-Singh@users.noreply.github.com>
2024-02-24 16:49:42 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors =  require('cors');
+
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-mongoose.connect('mongodb+srv://himanshusinghpurnea:Hs532002@cluster0.plypueb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb+srv://himanshusinghpurnea:Hs532002@cluster0.plypueb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const sensorDataSchema = new mongoose.Schema({
   value: Number,
@@ -14,30 +15,22 @@ const sensorDataSchema = new mongoose.Schema({
 
 const SensorData = mongoose.model('SensorData', sensorDataSchema);
 
-// app.post('/data', (req, res) => {
-//   const data = new SensorData({ value: req.body.sensorData });
-//   data.save(err => {
-//     if (err) return res.send(500, { error: err });
-//     return res.sendStatus(200);
-//   });
-// });
 app.post('/data', async (req, res) => {
-    try {
-        console.log(req.body);
-      const { sensorData } = req.body;
-      if (!sensorData) {
-        return res.status(400).json({ error: 'Sensor data is required' });
-      }
-  
-      const data = new SensorData({ value: sensorData });
-      await data.save();
-      
-      return res.sendStatus(200);
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Internal server error' });
+  try {
+    console.log(req.body);
+    const { sensorData } = req.body;
+    if (sensorData === null || sensorData === undefined) {
+      return res.status(400).json({ error: 'Sensor data is required' });
     }
-  });
+    const _value = parseFloat(sensorData)
+    const data = new SensorData({ value: _value });
+    await data.save();
 
+    return res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 app.listen(3000, () => console.log('Server started on port 3000'));
